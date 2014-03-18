895da1836d8e1519ab9eb99a9293c0d0048f30f6
Virantha Ekanayake <virantha@gmail.com>
2014-03-18 15:26:53 -0400

    first steps

--- scanpdf/scanpdf.py
+++ scanpdf/scanpdf.py
@@ -12,6 +12,19 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Scan to PDF.
+
+Usage:
+    scanpdf [options] scan <pdffile>
+
+
+Options:
+    -v --verbose    Verbose logging
+    -d --debug      Debug logging
+    --dpi=<dpi>     DPI to scan in [default: 300]
+    --tmpdir=<dir>  Temporary directory [default: /tmp]
+
+"""
 
 import argparse
 import sys, os
@@ -20,13 +33,12 @@ import shutil
 
 from version import __version__
 import yaml
+import docopt
+
+import subprocess
+import time
 
 
-"""
-   
-.. automodule:: scanpdf
-    :private-members:
-"""
 
 class ScanPdf(object):
     """
@@ -39,18 +51,11 @@ class ScanPdf(object):
         """
         self.config = None
 
-    def _get_config_file(self, config_file):
-        """
-           Read in the yaml config file
-
-           :param config_file: Configuration file (YAML format)
-           :type config_file: file
-           :returns: dict of yaml file
-           :rtype: dict
-        """
-        with config_file:
-            myconfig = yaml.load(config_file)
-        return myconfig
+    def cmd(self, cmd_list):
+        if isinstance(cmd_list, list):
+            cmd_list = ' '.join(cmd_list)
+        out = subprocess.check_output(cmd_list, stderr=subprocess.STDOUT, shell=True)
+        return out
 
 
     def get_options(self, argv):
@@ -65,83 +70,23 @@ class ScanPdf(object):
             :ivar config: Dict of the config file
 
         """
-        p = argparse.ArgumentParser(
-                description = "",
-                epilog = "Scan PDF version %s (Copyright 2014 Virantha Ekanayake)" % __version__,
-                )
+        self.args = argv
 
-        p.add_argument('-d', '--debug', action='store_true',
-            default=False, dest='debug', help='Turn on debugging')
-
-        p.add_argument('-v', '--verbose', action='store_true',
-            default=False, dest='verbose', help='Turn on verbose mode')
-
-        p.add_argument('-m', '--mail', action='store_true',
-            default=False, dest='mail', help='Send email after conversion')
-
-        #---------
-        # Single or watch mode
-        #--------
-        single_or_watch_group = p.add_mutually_exclusive_group(required=True)
-        # Positional argument for single file conversion
-        single_or_watch_group.add_argument("pdf_filename", nargs="?", help="Scanned pdf file to OCR")
-        # Watch directory for watch mode
-        single_or_watch_group.add_argument('-w', '--watch', 
-             dest='watch_dir', help='Watch given directory and run ocr automatically until terminated')
-
-        #-----------
-        # Filing options
-        #----------
-        filing_group = p.add_argument_group(title="Filing optinos")
-        filing_group.add_argument('-f', '--file', action='store_true',
-            default=False, dest='enable_filing', help='Enable filing of converted PDFs')
-        filing_group.add_argument('-c', '--config', type = argparse.FileType('r'),
-             dest='configfile', help='Configuration file for defaults and PDF filing')
-        filing_group.add_argument('-e', '--evernote', action='store_true',
-            default=False, dest='enable_evernote', help='Enable filing to Evernote')
-
-
-        args = p.parse_args(argv)
-
-        self.debug = args.debug
-        self.verbose = args.verbose
-        self.pdf_filename = args.pdf_filename
-        self.watch_dir = args.watch_dir
-        self.enable_email = args.mail
-
-        if self.debug:
-            logging.basicConfig(level=logging.DEBUG, format='%(message)s')
-
-        if self.verbose:
+        if argv['--verbose']:
             logging.basicConfig(level=logging.INFO, format='%(message)s')
+        if argv['--debug']:
+            logging.basicConfig(level=logging.DEBUG, format='%(message)s')                
+        self.pdf_filename = self.args['<pdffile>']
+        self.dpi = self.args['--dpi']
 
-        # Parse configuration file (YAML) if specified
-        if args.configfile:
-            self.config = self._get_config_file(args.configfile)
-            logging.debug("Read in configuration file")
-            logging.debug(self.config)
-
-        if args.enable_evernote:
-            self.enable_evernote = True
+        output_dir = time.strftime('%Y-%m-%d_%H%M.%S', time.localtime())
+        self.tmp_dir = os.path.join(self.args['--tmpdir'], output_dir)
+        self.tmp_dir = os.path.abspath(self.tmp_dir)
+        if os.path.exists(self.tmp_dir):
+            self._error("Temporary output directory %s already exists!" % self.tmp_dir)
+            sys.exit(-1)
         else:
-            self.enable_evernote = False
-
-        if args.enable_filing or args.enable_evernote:
-            self.enable_filing = True
-            if not args.configfile:
-                p.error("Please specify a configuration file(CONFIGFILE) to enable filing")
-        else:
-            self.enable_filing = False
-
-        self.watch = False
-
-        if args.watch_dir:
-            logging.debug("Starting to watch")
-            self.watch = True
-
-        if self.enable_email:
-            if not args.configfile:
-                p.error("Please specify a configuration file(CONFIGFILE) to enable email")
+            os.makedirs(self.tmp_dir)
 
 
     def go(self, argv):
@@ -153,11 +98,17 @@ class ScanPdf(object):
         """
         # Read the command line options
         self.get_options(argv)
+        logging.info("Temp dir: %s" % self.tmp_dir)
+        self.cmd(['ls', '-h', '-l'])
+
+
 
 
 def main():
+    args = docopt.docopt(__doc__, version='Scan PDF %s' % __version__ )
     script = ScanPdf()
-    script.go(sys.argv[1:])
+    print args
+    script.go(args)
 
 if __name__ == '__main__':
     main()
